ef7323fa32e5df830c4f8d8c52536244194d6a05
xyq <abaddonpoet@gmail.com>
2021-09-28 09:53:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<!--<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+</template>
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="My first Online Vue Page" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
